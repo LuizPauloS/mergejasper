96bfa39c86f15eac30c12ec69b95be4e5ec64ee5
Luiz <luiz.paulo.09@hotmail.com>
2020-05-28 10:10:20 -0400

    #Commitando implementação de método de imprimir imagens pdf

--- src/main/java/br/com/lsilva/mergejasperdemo/controller/TesteController.java
+++ src/main/java/br/com/lsilva/mergejasperdemo/controller/TesteController.java
@@ -18,7 +18,7 @@ public class TesteController {
 
     @GetMapping
     public ResponseEntity<?> getPdf() throws Exception {
-        byte[] documento = this.service.createIndexPDF();
+        byte[] documento = this.service.manipulatePdf();
         HttpHeaders headers = new HttpHeaders();
         headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
         headers.add("Pragma", "no-cache");
--- src/main/java/br/com/lsilva/mergejasperdemo/service/TesteService.java
+++ src/main/java/br/com/lsilva/mergejasperdemo/service/TesteService.java
@@ -1,34 +1,25 @@
 package br.com.lsilva.mergejasperdemo.service;
 
-import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.forms.PdfPageFormCopier;
 import com.itextpdf.io.image.ImageData;
 import com.itextpdf.io.image.ImageDataFactory;
-import com.itextpdf.kernel.font.PdfFont;
-import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.*;
 import com.itextpdf.kernel.pdf.action.PdfAction;
-import com.itextpdf.kernel.pdf.canvas.draw.DottedLine;
+import com.itextpdf.kernel.pdf.canvas.draw.DashedLine;
 import com.itextpdf.kernel.pdf.navigation.PdfDestination;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.*;
-import com.itextpdf.layout.layout.LayoutContext;
-import com.itextpdf.layout.layout.LayoutResult;
-import com.itextpdf.layout.property.AreaBreakType;
 import com.itextpdf.layout.property.TabAlignment;
 import com.itextpdf.layout.property.TextAlignment;
 import com.itextpdf.layout.property.VerticalAlignment;
-import com.itextpdf.layout.renderer.ParagraphRenderer;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
-import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.AbstractMap.SimpleEntry;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.List;
 
 @Service
@@ -53,25 +44,7 @@ public class TesteService {
         return byteOutputStream.toByteArray();
     }
 
-    public byte[] getImagesFormat() throws IOException {
-//        ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
-//        String [] IMAGES = {
-//                new ClassPathResource(IMG_SRC + "cpf.jpg").getPath(),
-//                new ClassPathResource(IMG_SRC + "rg.jpg").getPath()
-//        };
-//        Image image = new Image(ImageDataFactory.create(IMAGES[0]));
-//        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(byteOutputStream));
-//        Document doc = new Document(pdfDoc, PageSize.A4);
-//
-//        for (int i = 0; i < IMAGES.length; i++) {
-//            image = new Image(ImageDataFactory.create(IMAGES[i]));
-//            pdfDoc.addNewPage(PageSize.A4);
-//            //image.setFixedPosition(i + 1, 0, 0);
-//            doc.add(image);
-//        }
-//
-//        doc.close();
-//        return byteOutputStream.toByteArray();
+    public List<byte[]> getImagesFormat() throws IOException {
         byte[] img = Files.readAllBytes(Paths.get(new ClassPathResource(IMG_SRC + "cpf.jpg").getURI()));
         byte[] img2 = Files.readAllBytes(Paths.get(new ClassPathResource(IMG_SRC + "rg.jpg").getURI()));
         byte[] img3 = Files.readAllBytes(Paths.get(new ClassPathResource(IMG_SRC + "brasao_ms.jpg").getURI()));
@@ -81,133 +54,107 @@ public class TesteService {
         byte[] img7 = Files.readAllBytes(Paths.get(new ClassPathResource(IMG_SRC + "relatorio_logo.jpg").getURI()));
         byte[] img8 = Files.readAllBytes(Paths.get(new ClassPathResource(IMG_SRC + "teste.jpg").getURI()));
 
-        return merge(img, img2, img3, img4, img5, img6, img7, img8);
+        return Arrays.asList(img, img2, img3, img4, img5, img6, img7, img8);
     }
 
-    public byte[] createIndexPDF() throws Exception {
-        ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
-        PdfFont font = PdfFontFactory.createFont(StandardFonts.TIMES_ROMAN);
-        PdfFont bold = PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);
+    public byte[] manipulatePdf() throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(baos));
+        Document doc = new Document(pdfDoc);
 
-        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(byteOutputStream));
-        Document document = new Document(pdfDoc);
+        // Copier contains the additional logic to copy acroform fields to a new page.
+        // PdfPageFormCopier uses some caching logic which can potentially improve performance
+        // in case of the reusing of the same instance.
+        PdfPageFormCopier formCopier = new PdfPageFormCopier();
 
-        document.setTextAlignment(TextAlignment.JUSTIFIED)
-                .setFont(font)
-                .setFontSize(11);
-        List<SimpleEntry<String, SimpleEntry<String, Integer>>> toc = new ArrayList<>();
+        // Copy all merging file's pages to the temporary pdf file
+        Map<String, PdfDocument> filesToMerge = initializeFilesToMerge(getImagesFormat());
+        Map<Integer, String> toc = new TreeMap<>();
+        int page = 1;
+        for (Map.Entry<String, PdfDocument> entry : filesToMerge.entrySet()) {
+            PdfDocument srcDoc = entry.getValue();
+            int numberOfPages = srcDoc.getNumberOfPages();
 
-        // Parse text to PDF
-        createPdfWithOutlines(TXT_SRC, document, toc, bold);
+            toc.put(page, entry.getKey());
 
-        // Remove the main title from the table of contents list
-        toc.remove(0);
+            for (int i = 1; i <= numberOfPages; i++, page++) {
+                Text text = new Text(String.format("Page %d", page));
+                srcDoc.copyPagesTo(i, i, pdfDoc, formCopier);
 
-        // Create table of contents
-        document.add(new AreaBreak());
-        Paragraph p = new Paragraph("Table of Contents")
-                .setFont(bold)
-                .add(new Tab());
-        document.add(p);
+                // Put the destination at the very first page of each merged document
+                if (i == 1) {
+                    text.setDestination("p" + page);
 
-        List<TabStop> tabStops = new ArrayList<>();
-        tabStops.add(new TabStop(580, TabAlignment.RIGHT, new DottedLine()));
-
-        for (SimpleEntry<String, SimpleEntry<String, Integer>> entry : toc) {
-            SimpleEntry<String, Integer> text = entry.getValue();
-            p = new Paragraph()
-                    .addTabStops(tabStops)
-                    .add(text.getKey())
-                    .add(new Tab())
-                    .add(String.valueOf(text.getValue()))
-                    .setAction(PdfAction.createGoTo(entry.getKey()));
-            document.add(p);
-        }
-
-        // Move the table of contents to the first page
-        int tocPageNumber = pdfDoc.getNumberOfPages();
-        pdfDoc.movePage(tocPageNumber, 1);
-
-        // Add page labels
-        pdfDoc.getPage(1).setPageLabel(PageLabelNumberingStyle.UPPERCASE_LETTERS,
-                null, 1);
-        pdfDoc.getPage(2).setPageLabel(PageLabelNumberingStyle.DECIMAL_ARABIC_NUMERALS,
-                null, 1);
-        document.close();
-        return byteOutputStream.toByteArray();
-    }
-
-    private void createPdfWithOutlines(String path, Document document,
-                                       List<SimpleEntry<String, SimpleEntry<String, Integer>>> toc,
-                                       PdfFont titleFont) throws Exception {
-        PdfDocument pdfDocument = document.getPdfDocument();
-
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String line;
-            boolean title = true;
-            int counter = 0;
-            PdfOutline outline = null;
-            while ((line = br.readLine()) != null) {
-                Paragraph p = new Paragraph(line);
-                p.setKeepTogether(true);
-                if (title) {
-                    String name = String.format("title%02d", counter++);
-                    outline = createOutline(outline, pdfDocument, line, name);
-                    SimpleEntry<String, Integer> titlePage = new SimpleEntry(line, pdfDocument.getNumberOfPages());
-                    p.setFont(titleFont)
-                        .setFontSize(12)
-                        .setKeepWithNext(true)
-                        .setDestination(name)
-                        // Add the current page number to the table of contents list
-                        .setNextRenderer(new UpdatePageRenderer(p, titlePage));
-                    document.add(p);
-                    toc.add(new SimpleEntry(name, titlePage));
-                    title = false;
-                } else {
-                    p.setFirstLineIndent(36);
-                    if (line.isEmpty()) {
-                        p.setMarginBottom(12);
-                        title = true;
-                    } else {
-                        p.setMarginBottom(0);
-                    }
-
-                    document.add(p);
+                    PdfOutline rootOutLine = pdfDoc.getOutlines(false);
+                    PdfOutline outline = rootOutLine.addOutline("p" + page);
+                    outline.addDestination(PdfDestination.makeDestination(new PdfString("p" + page)));
                 }
+
+                doc.add(new Paragraph(text)
+                        .setFixedPosition(page, 549, 810, 40)
+                        .setMargin(0)
+                        .setMultipliedLeading(1));
             }
         }
-    }
 
-    private PdfOutline createOutline(PdfOutline outline, PdfDocument pdf, String title, String name) {
-        if (outline == null) {
-            outline = pdf.getOutlines(false);
-            outline = outline.addOutline(title);
-            outline.addDestination(PdfDestination.makeDestination(new PdfString(name)));
-        } else {
-            PdfOutline kid = outline.addOutline(title);
-            kid.addDestination(PdfDestination.makeDestination(new PdfString(name)));
+        PdfDocument tocDoc = new PdfDocument(new PdfWriter(baos));
+        tocDoc.copyPagesTo(1, 1, pdfDoc, formCopier);
+        tocDoc.close();
+
+        // Create a table of contents
+        float tocYCoordinate = 750;
+        float tocXCoordinate = doc.getLeftMargin();
+        float tocWidth = pdfDoc.getDefaultPageSize().getWidth() - doc.getLeftMargin() - doc.getRightMargin();
+        for (Map.Entry<Integer, String> entry : toc.entrySet()) {
+            Paragraph p = new Paragraph();
+            p.addTabStops(new TabStop(500, TabAlignment.LEFT, new DashedLine()));
+            p.add(entry.getValue());
+            p.add(new Tab());
+            p.add(String.valueOf(entry.getKey()));
+            p.setAction(PdfAction.createGoTo("p" + entry.getKey()));
+            doc.add(p
+                    .setFixedPosition(pdfDoc.getNumberOfPages(), tocXCoordinate, tocYCoordinate, tocWidth)
+                    .setMargin(0)
+                    .setMultipliedLeading(1));
+
+            tocYCoordinate -= 20;
         }
 
-        return outline;
-    }
-
-    private static class UpdatePageRenderer extends ParagraphRenderer {
-        protected SimpleEntry<String, Integer> entry;
-
-        public UpdatePageRenderer(Paragraph modelElement, SimpleEntry<String, Integer> entry) {
-            super(modelElement);
-            this.entry = entry;
+        for (PdfDocument srcDoc : filesToMerge.values()) {
+            srcDoc.close();
         }
 
-        @Override
-        public LayoutResult layout(LayoutContext layoutContext) {
-            LayoutResult result = super.layout(layoutContext);
-            entry.setValue(layoutContext.getArea().getPageNumber());
-            return result;
+        doc.close();
+
+        PdfDocument resultDoc = new PdfDocument(new PdfWriter(baos));
+        PdfDocument srcDoc = new PdfDocument(new PdfReader(new ByteArrayInputStream(baos.toByteArray()),
+                new ReaderProperties()));
+        srcDoc.initializeOutlines();
+
+        // Create a copy order list and set the page with a table of contents as the first page
+        int tocPageNumber = srcDoc.getNumberOfPages();
+        List<Integer> copyPagesOrderList = new ArrayList<>();
+        copyPagesOrderList.add(tocPageNumber);
+        for(int i = 1; i < tocPageNumber; i++) {
+            copyPagesOrderList.add(i);
         }
+
+        srcDoc.copyPagesTo(copyPagesOrderList, resultDoc, formCopier);
+
+        srcDoc.close();
+        resultDoc.close();
+        return baos.toByteArray();
     }
 
-    public byte[] merge(byte[]... documents) {
+    private Map<String, PdfDocument> initializeFilesToMerge(List<byte[]> documents) throws IOException {
+        Map<String, PdfDocument> filesToMerge = new TreeMap<>();
+        for (int i = 0; i < documents.size(); i++) {
+            filesToMerge.put("Testando N° "+i, getPdfDocumentImage(documents.get(i)));
+        }
+        return filesToMerge;
+    }
+
+    public PdfDocument getPdfDocumentImage(byte[] image) {
         ByteArrayOutputStream result = new ByteArrayOutputStream();
         PdfWriter pdfWriter = new PdfWriter(result);
         PdfDocument copy = new PdfDocument(new PdfWriter(pdfWriter));
@@ -218,22 +165,15 @@ public class TesteService {
         float pdfA4usableWidth = PageSize.A4.getWidth() - leftMargin - rightMargin;
         float pdfA4usableHeight = PageSize.A4.getHeight() - topMargin - bottomMargin;
 
-        for (int i = 0; i < documents.length; i++) {
-            ImageData imageData = ImageDataFactory.create(documents[i]);
-            Image image = new Image(imageData);
-            image.scaleToFit(pdfA4usableWidth, pdfA4usableHeight);
-            float x = (PageSize.A4.getWidth() - image.getImageScaledWidth()) / 2;
-            float y = (PageSize.A4.getHeight() - image.getImageScaledHeight()) / 2;
-            image.setFixedPosition(i+1, x, y);
-            document.add(image);
-        }
+        ImageData imageData = ImageDataFactory.create(image);
+        Image img = new Image(imageData);
+        img.scaleToFit(pdfA4usableWidth, pdfA4usableHeight);
+        float x = (PageSize.A4.getWidth() - img.getImageScaledWidth()) / 2;
+        float y = (PageSize.A4.getHeight() - img.getImageScaledHeight()) / 2;
+        img.setFixedPosition(x, y);
+        document.add(img);
 
-        int numberOfPages = copy.getNumberOfPages();
-        for (int i = 1; i <= numberOfPages; i++) {
-            document.showTextAligned(new Paragraph(String.format("%s", i)),
-                    559, 806, i, TextAlignment.RIGHT, VerticalAlignment.TOP, 0);
-        }
         document.close();
-        return result.toByteArray();
+        return copy;
     }
 }
